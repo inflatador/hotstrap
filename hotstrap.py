ce92a8e2ba36711a9b0f46f58de0e6521e60abc9
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2019-07-09 14:16:51 -0500

    Adding some error logic for jenkins to pick up on

--- hotstrap.py
+++ hotstrap.py
@@ -26,8 +26,11 @@ def install_packages():
             print('Installing ' + package)
             os.system('yum install -y ' + package + '>/dev/null')
             print('Successful\n')
+            did_package = True
+            return did_package
     except:
         print('Unsuccessful')
+        exit(1)
 
 
 # Install required packages via pip
@@ -41,26 +44,35 @@ def pip_down():
     try:
         print('Installing decorator')
         os.system('pip install -U decorator >/dev/null')
+        print('Installing ansible')
+        os.system('pip install ansible==2.4.3.0 > /dev/null')
         for package in os_list:
             print('Installing ' + package)
             os.system('pip install ' + package + '>/dev/null')
             print('Successful')
-        print('Installing ansible')
-        os.system('pip install ansible==2.4.3.0 > /dev/null')
+        did_pip = True
+        return did_pip
     except:
         print('Unsuccessful')
+        exit(1)
 
 
 # Remove git repo if it exist (should never come up but might as well)
 # Clone git repo that has all our configuration files
 def git_configuration():
-    import git
     try:
-        shutil.rmtree('hotstrap/')
-    except OSError:
-        pass
-    print('\nCloning down configuration files')
-    git.Git('./').clone('git://github.com/kmcjunk/hotstrap.git')
+        import git
+        try:
+            shutil.rmtree('hotstrap/')
+        except OSError:
+            pass
+        print('\nCloning down configuration files')
+        git.Git('./').clone('git://github.com/kmcjunk/hotstrap.git')
+        did_git = True
+        return did_git
+    except:
+        print('Git configuration failure')
+        exit(1)
 
 
 # Move configuration files to the proper location on the OS
@@ -77,48 +89,65 @@ def configurate():
                  'usr/libexec/os-apply-config/templates/var/run/heat-config/heat-config',
                  'usr/libexec/os-apply-config/templates/etc/os-collect-config.conf']
     print('Moving configuration files to the proper locations\n\n')
-    for file in file_list:
-        directory = os.path.dirname('/' + file)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        print('hotstrap/' + file + '\t->\t' + '/' + file)
-        shutil.move('hotstrap/' + file, '/' + file)
-    for i in range(3):
-        os.chmod('/' + file_list[i], 0700)
-    for i in range(3, 6):
-        os.chmod('/' + file_list[i], 0755)
-        # os.chmod('/' + file, 0700)
+    try:
+        for file in file_list:
+            directory = os.path.dirname('/' + file)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            print('hotstrap/' + file + '\t->\t' + '/' + file)
+            shutil.move('hotstrap/' + file, '/' + file)
+        for i in range(3):
+            os.chmod('/' + file_list[i], 0700)
+        for i in range(3, 6):
+            os.chmod('/' + file_list[i], 0755)
+        did_configure = True
+        return did_configure
+    except:
+        print('Configurate failure')
+        exit(1)
 
 
 # Run os-collect to propagate the config & run it again
 # Then run start_config to create/enable the os-collect service
 # Also clean up the git repo cause it is dead to us
 def jiggle_some_things():
-    print('\nRunning os-collect-config & ensuring os-collect-config-exist')
-    os.system('os-collect-config --one-time --debug')
-    os.system('cat /etc/os-collect-config.conf')
-    os.system('os-collect-config --one-time --debug')
-    print('\nEnsuring everything is running & enabled on boot')
-    subprocess.call('hotstrap/start_config_agent.sh')
-    print('\nCleaning up git folder')
-    shutil.rmtree('hotstrap/')
+    try:
+        print('\nRunning os-collect-config & ensuring os-collect-config-exist')
+        os.system('os-collect-config --one-time --debug')
+        os.system('cat /etc/os-collect-config.conf')
+        os.system('os-collect-config --one-time --debug')
+        print('\nEnsuring everything is running & enabled on boot')
+        subprocess.call('hotstrap/start_config_agent.sh')
+        print('\nCleaning up git folder')
+        shutil.rmtree('hotstrap/')
+        did_jiggle = True
+        return did_jiggle
+    except:
+        print('Jiggle failure')
+        exit(1)
 
 
 # Ensure we don't get rekt by cloud-init next boot
 def delete_some_other_things():
-    print('Ensuring no cloud-init references exist')
-    os.system('rm -rf /var/lib/cloud/instance')
-    os.system('rm -rf /var/lib/cloud/instances/*')
-    os.system('rm -rf /var/lib/cloud/data/*')
-    os.system('rm -rf /var/lib/cloud/sem/config_scripts_per_once.once')
-    os.system('rm -rf /var/log/cloud-init.log')
-    os.system('rm -rf /var/log/cloud-init-output.log')
-    print('\n\n\nDone!')
+    try:
+        print('Ensuring no cloud-init references exist')
+        os.system('rm -rf /var/lib/cloud/instance')
+        os.system('rm -rf /var/lib/cloud/instances/*')
+        os.system('rm -rf /var/lib/cloud/data/*')
+        os.system('rm -rf /var/lib/cloud/sem/config_scripts_per_once.once')
+        os.system('rm -rf /var/log/cloud-init.log')
+        os.system('rm -rf /var/log/cloud-init-output.log')
+        print('\n\n\nDone!')
+        did_delete = True
+        return did_delete
+    except:
+        print('Delete failure')
+        exit(1)
 
 
-install_packages()
-pip_down()
-git_configuration()
-configurate()
-jiggle_some_things()
-delete_some_other_things()
+did_package = install_packages()
+did_pip = pip_down()
+did_git = git_configuration()
+did_configure = configurate()
+did_jiggle = jiggle_some_things()
+did_delete = delete_some_other_things()
