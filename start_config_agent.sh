1059060c417a0b9941ae1f5437529b5ab5845557
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2019-07-15 12:45:14 -0500

    Chopped out some if statements for initd as it was causing issues with the way kickstart chroots(?). Note: Integrate start_config_agent.sh into hotstrap.py

--- start_config_agent.sh
+++ start_config_agent.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -eux
 
-if [[ `systemctl` =~ -\.mount ]]; then
+# if there is no system unit file, install a local unit
+if [ ! -f /usr/lib/systemd/system/os-collect-config.service ]; then
 
-    # if there is no system unit file, install a local unit
-    if [ ! -f /usr/lib/systemd/system/os-collect-config.service ]; then
-
-        cat <<EOF >/etc/systemd/system/os-collect-config.service
+    cat <<EOF >/etc/systemd/system/os-collect-config.service
 [Unit]
 Description=Collect metadata and run hook commands.
 
@@ -24,28 +22,6 @@ command=os-refresh-config
 EOF
     fi
 
-    # enable and start service to poll for deployment changes
-    systemctl enable os-collect-config
-    systemctl start --no-block os-collect-config
-elif [[ `/sbin/init --version` =~ upstart ]]; then
-    if [ ! -f /etc/init/os-collect-config.conf ]; then
-
-        cat <<EOF >/etc/init/os-collect-config.conf
-start on runlevel [2345]
-stop on runlevel [016]
-respawn
-
-# We're logging to syslog
-console none
-
-exec os-collect-config  2>&1 | logger -t os-collect-config
-EOF
-    fi
-    initctl reload-configuration
-    service os-collect-config start
-else
-    echo "ERROR: only systemd or upstart supported" 1>&2
-    exit 1
-fi
-
-
+# enable and start service to poll for deployment changes
+systemctl enable os-collect-config
+systemctl start --no-block os-collect-config
