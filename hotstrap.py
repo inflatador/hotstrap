1c7291d57f9afd7b7255fc20a2dbee7e42c13baf
Rocky McCamey <rockymccamey@gmail.com>
2020-01-22 15:44:11 -0600

    installing python

--- hotstrap.py
+++ hotstrap.py
@@ -10,7 +10,8 @@ import shutil
 
 # Install required packages via yum
 def install_packages():
-    package_list = ['python-pip',
+    package_list = ['python'
+                    'python-pip',
                     'gcc',
                     'git',
                     'python-dev',
